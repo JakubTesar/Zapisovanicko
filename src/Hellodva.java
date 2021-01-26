c5de1ab9484dccef6bd30dafd19fa727899315d7
JakubTesar <jakub.tesar@educanet.cz>
2021-01-26 12:39:33 +0100

    ahojsvěte100krát

--- src/Hellodva.java
+++ src/Hellodva.java
@@ -1,5 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Hellodva {
     public static void main(String[] args) {
-        
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("hellosvete100.txt"));
+
+            for (int i = 0; i < 100; i++) {
+                bw.write("Hello světe 100!");
+                bw.newLine();
+            }
+
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
